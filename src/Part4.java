137e89bc2c0b99a44a57fea0e2d8979e7e9f7c71
Noor Ul Islam <noorulislam770@gmail.com>
2024-10-23 16:53:51 +0500

    solved the links finding task with onyl string methods without using pattern matching etc

--- src/Part4.java
+++ src/Part4.java
@@ -2,66 +2,59 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URLConnection;
 import  java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Part4 {
     public static void main(String[] args) {
-    try {
+        try {
 
 
-        URL url = new URL("https://www.dukelearntoprogram.com/course2/data/manylinks.html");
-        URLConnection urlConnection = url.openConnection();
-        BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            URL url = new URL("https://www.dukelearntoprogram.com/course2/data/manylinks.html");
+            URLConnection urlConnection = url.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 
-        String inputLine ;
-        StringBuilder content = new StringBuilder();
-        Pattern hrefPattern = Pattern.compile("href\\s*=\\s*\"([^\"]*)\"");
-
-        while ((inputLine = in.readLine()) != null) {
-//            System.out.println(inputLine);
-            Matcher hrefMatcher = hrefPattern.matcher(inputLine);
-            while (hrefMatcher.find()) {
-                content.append(hrefMatcher.group(1)).append("\n");
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+    //            System.out.println(inputLine);
+                content.append(inputLine);
             }
 
+            in.close();
+            String rawContent = content.toString().toLowerCase();
+    //        System.out.println(rawContent);
+    //        System.out.println(content.toString());
+
+            List<String> links = new ArrayList<>();
+
+            while (true) {
+
+                boolean containsLink = rawContent.contains("youtube.com");
+    //            System.out.println(containsLink);
+                if (containsLink) {
+                    int startIndex = rawContent.indexOf("youtube.com");
+                    int endIndex = rawContent.indexOf("\"", startIndex);
+                    String link = rawContent.substring(startIndex, endIndex);
+                    link = "www." + link;
+                    links.add(link);
+                    rawContent = rawContent.substring(endIndex+1);
+                }
+                else {
+                    break;
+            }
+
+    //        System.out.println(rawContent);
+            for (int i =0; i < links.size(); i++ ) {
+                String link = links.get(i);
+                System.out.println(link);
+            }
         }
-
-        in.close();
-
-
-    String pageContent = content.toString();
-    Pattern pattern = Pattern.compile("www\\.youtube\\.com");
-    Matcher matcher = pattern.matcher(pageContent);
-    boolean found = false;
-
-    while (matcher.find()) {
-        int start = matcher.start();
-        int end = pageContent.indexOf("\n", start);
-        if (end == -1) {
-            end = pageContent.length() ;
+        }catch (Exception e) {
+            e.printStackTrace();
         }
-        System.out.println(pageContent.substring(start,end ));
-        found = true;
-
     }
-
-    if (!found) {
-        System.out.println("No match found");
-    }
-
-
-
-    }catch (Exception e) {
-        e.printStackTrace();
-    }
-    }
-
-
-
-
-
-
-
 
 }
